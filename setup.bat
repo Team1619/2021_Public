c73f3b90516150840a48a278e7a195674f7f7ea3
CaitlynFong <44216546+CaitlynFong@users.noreply.github.com>
2021-12-23 20:34:16 -0700

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,10 @@
+git clone https://github.com/Team1619/uacr-robot-core.git
+git clone https://github.com/Team1619/uacr-pure-pursuit.git
+git clone https://github.com/Team1619/uacr-robot-dashboard.git
+git clone https://github.com/Team1619/frc-core-plugin.git
+git clone https://github.com/Team1619/2020-frc-compbot.git
+git clone https://github.com/Team1619/2020-frc-protobot.git
+git clone https://github.com/Team1619/2020-frc-basebot.git
+git clone https://github.com/Team1619/2021-frc-challengebot.git
+git clone https://github.com/Team1619/2021-frc-defensebot.git
+git clone https://github.com/Team1619/2021-frc-t-shirt-bot.git
